4f93ff69571cf82497590c47687cca57af967d4f
Ben Kerllenevich <me@notom3ga.me>
2021-05-13 19:36:57 -0400

    Initial Commit

---
+++ subprojects/server.gradle.kts
@@ -0,0 +1,9 @@
+repositories {
+    maven("https://jitpack.io") {
+        content { includeGroupByRegex("com\\.github\\..*") }
+    }
+}
+
+dependencies {
+    implementation("cat.inspiracio", "rhino-js-engine", "1.7.7.1")
+}
